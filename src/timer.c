d9c4dfede7228e28e2fdd405a5b971cc3f618016
raphaelcoeffic <raphael.coeffic@frafos.com>
2025-04-10 08:39:43 +0200

    feat: free running 32bit 48 MHz timer

--- src/timer.c
+++ src/timer.c
@@ -12,32 +12,44 @@ static void timer0_int_handler() {
 
 static inline uint16_t _read_micros() { return HWREGH(GPT0_BASE + GPT_O_TAR); }
 
-// Uses GPT0-A
+// Uses:
+// - GPT0-A: millis() & micros()
+// - GPT1: free running @ 48MHz
+// 
 void timer_init()
 {
   PRCMPeripheralRunEnable(PRCM_PERIPH_TIMER0);
+  PRCMPeripheralRunEnable(PRCM_PERIPH_TIMER1);
   PRCMLoadSet();
 
+  // GPT0-A
   TimerLoadSet(GPT0_BASE, TIMER_A, 1000 - 1);
   TimerPrescaleSet(GPT0_BASE, TIMER_A, 48 - 1);
   TimerConfigure(GPT0_BASE, TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_PERIODIC);
   TimerIntRegister(GPT0_BASE, TIMER_A, timer0_int_handler);
   TimerIntEnable(GPT0_BASE, TIMER_TIMA_TIMEOUT);
   TimerEnable(GPT0_BASE, TIMER_A);
+
+  // GPT1
+  TimerConfigure(GPT1_BASE, TIMER_CFG_PERIODIC_UP);
+  TimerEnable(GPT1_BASE, TIMER_BOTH);
 }
 
 uint32_t millis() { return _ms_ticks; }
 
 uint32_t micros() {
-  uint32_t ms, us;
+  uint32_t ms, us, ms2;
 
   do {
     ms = _ms_ticks;
     us = _read_micros();
-    __asm__("nop \n"
-            "nop \n");
-  } while (ms != _ms_ticks);
+    ms2 = _ms_ticks;
+  } while (ms != ms2);
 
   return (ms + 1) * 1000 - us;
 }
 
+uint32_t get_ticks() {
+  // GPT1: 32bit combined access
+  return HWREG(GPT1_BASE + GPT_O_TAR);
+}
--- src/timer.h
+++ src/timer.h
@@ -2,8 +2,31 @@
 
 #include <stdint.h>
 
-// Init timer 0 A
+// Init GPT0-A and GPT1
 void timer_init();
 
+// Get milliseconds
 uint32_t millis();
+
+// Get microseconds
 uint32_t micros();
+
+// Get ticks timer (48 MHz)
+uint32_t get_ticks();
+
+// Convert microseconds into ticks
+#define us2ticks(us) (us * 48)
+
+// Comparison macros
+#define _timer_diff(t1, t2) ((int32_t)(t1 - t2))
+#define _timer_before(t1, t2) (_timer_diff(t1, t2) < 0)
+#define _timer_after(t1, t2) (_timer_diff(t1, t2) >= 0)
+
+#define millis_before(ms) _timer_before(millis(), ms)
+#define millis_after(ms) _timer_after(millis(), ms)
+
+#define micros_before(ms) _timer_before(micros(), ms)
+#define micros_after(ms) _timer_after(micros(), ms)
+
+#define ticks_before(ms) _timer_before(get_ticks(), ms)
+#define ticks_after(ms) _timer_after(get_ticks(), ms)
